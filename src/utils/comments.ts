4e559c04b088e5c1f0bc90be2a35d808ba176a6e
jicjjang <jicjjang12@gmail.com>
2020-04-21 01:51:42 +0900

    chore: change branch name

--- src/utils/comments.ts
+++ src/utils/comments.ts
@@ -1,7 +1,7 @@
 export const addComments = () => {
   const utterancElement = document.createElement('script');
   utterancElement.setAttribute('src', 'https://utteranc.es/client.js');
-  utterancElement.setAttribute('repo', 'jicjjang/blog2');
+  utterancElement.setAttribute('repo', 'jicjjang/jicjjang.github.io');
   utterancElement.setAttribute('issue-term', 'pathname');
   utterancElement.setAttribute('label', '✨댓글✨');
   utterancElement.setAttribute('theme', 'github-light');
