f75a02e31ae543e05c86cab7cb90804e6c0e0d81
jicjjang <jicjjang12@gmail.com>
2020-05-24 19:00:30 +0900

    Updates

---
+++ component---src-pages-404-tsx-f47482f7c9152579fccf.js
@@ -0,0 +1,2 @@
+(window.webpackJsonp=window.webpackJsonp||[]).push([[2],{"i6+/":function(e,t,n){"use strict";n.r(t),n.d(t,"default",(function(){return r})),n.d(t,"pageQuery",(function(){return a}));var u=n("q1tI");function r(){return u.createElement(u.Fragment,null,u.createElement("h1",null,"Not Found"),u.createElement("p",null,"You just hit a route that doesn't exist... the sadness."))}var a="1097489062"}}]);
+//# sourceMappingURL=component---src-pages-404-tsx-f47482f7c9152579fccf.js.map
